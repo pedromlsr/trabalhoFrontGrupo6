25c40618a03f827a285315de4225cef571662ccf
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-05-12 02:30:21 -0300

    Resolvendo problemas no parallax e background

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,7 @@ let silhoutte2 = document.getElementById('silhoutte2');
 
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    backgroundParallax.style.top = value * 0.8 + 'px';
-    silhoutte.style.top = value * 0.5 + 'px';
-    silhoutte2.style.bottom = value * 1 + 'px';
-
+    backgroundParallax.style.top = value * 0.3 + 'px';
+    silhoutte.style.top = value * 0.4 + 'px';
+    silhoutte2.style.bottom = value * 0.2 + 'px';
 })
