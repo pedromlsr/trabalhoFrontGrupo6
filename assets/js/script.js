c0dd902302bcdd6a45ce02a772591282c540b37c
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-05-12 13:34:54 -0300

    Resolvido problema de layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 let backgroundParallax = document.getElementById('background-parallax');
 let silhoutte = document.getElementById('silhoutte');
-let silhoutte2 = document.getElementById('silhoutte2');
 
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    backgroundParallax.style.top = value * 0.3 + 'px';
-    silhoutte.style.top = value * 0.4 + 'px';
-    silhoutte2.style.bottom = value * 0.2 + 'px';
+    backgroundParallax.style.top = value * 0.5 + 'px';
+    silhoutte.style.top = value * 0.2 + 'px';
 })
