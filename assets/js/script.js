7028e877ff466446753132fb11309e6344dfb788
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-05-12 15:00:41 -0300

    Incluindo smoke e flare no parallax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 let backgroundParallax = document.getElementById('background-parallax');
 let silhoutte = document.getElementById('silhoutte');
+let smoke = document.getElementById('smoke');
+let flare = document.getElementById('flare');
 
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
     backgroundParallax.style.top = value * 0.5 + 'px';
     silhoutte.style.top = value * 0.2 + 'px';
+    smoke.style.top=value*0.5+'px';
+    flare.style.top=value*0.3+'px';
 })
